184296ac314d92edd723d1621605563314ee4a39
Minho Cha <minhocha1004@gmail.com>
2016-10-18 20:26:01 -0700

    Update MagazineView.java

--- MagazineView/MagazineView.java
+++ MagazineView/MagazineView.java
@@ -1,20 +1,8 @@
-/*
- * MagazineView.java
- * Creates the main frame of the program.
- *@author Lewis/Loftus
- *@modifer Ha Yee Chan, Minho Cha
- *@version 1.0
- *@since 2016-05-16
- */
-
 import javax.swing.JFrame;
 
-public class MagazineView
-{
+public class MagazineView{
    //  Creates the main frame of the program.
-
-   public static void main (String[] args)
-   {
+   public static void main (String[] args){
       JFrame frame = new JFrame ("MagazineView");
       frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 
@@ -23,6 +11,5 @@ public class MagazineView
       frame.getContentPane().add(panel);
       frame.pack();
       frame.setVisible(true);
-
    }
 }
