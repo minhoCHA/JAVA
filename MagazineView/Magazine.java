efb8929aeb2142489e351f26801820b9f24b0da5
Minho Cha <minhocha1004@gmail.com>
2016-10-18 20:25:35 -0700

    Update Magazine.java

--- MagazineView/Magazine.java
+++ MagazineView/Magazine.java
@@ -2,28 +2,21 @@
 /*
  * Magazine.java
  * Represents a single magazine.
- *@author Lewis/Loftus
- *@modifer Ha Yee Chan, Minho Cha
- *@version 1.0
- *@since 2016-05-16
+ * Minho Cha
+ * Date 2016-05-16
  */
 
 
-public class Magazine
-{
+public class Magazine{
    private String title;
 
    //  Sets up the new magazine with its title.
-
-   public Magazine (String newTitle)
-   {
+   public Magazine (String newTitle){
       title = newTitle;
    }
 
    //  Returns this magazine as a string.
-
-   public String toString ()
-   {
+   public String toString (){
       return title;
    }
 }
